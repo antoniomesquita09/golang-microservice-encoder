662873dd2b2619a83927f4717ca2ee53240a21fe
Antonio Mesquita <antonio.mesquita@ifood.com.br>
2024-01-10 11:11:54 -0300

    feat: video repository implementation

---
+++ application/repositories/video_repository_test.go
@@ -0,0 +1,28 @@
+package repositories
+
+import (
+	"encoder/domain"
+	"encoder/framework/database"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestNewVideoRepositoryDbInsert(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	repo := VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+
+	v, err := repo.Find(video.ID)
+	require.NotEmpty(t, v.ID)
+	require.Nil(t, err)
+	require.Equal(t, v.ID, video.ID)
+}
